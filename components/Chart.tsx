97c444626b0c8e9f6458acf10af39a622d87ea5e
arslanovngc <arslanov.uzinfocom@gmail.com>
2023-07-05 18:28:23 +0500

    some changes

--- components/Chart.tsx
+++ components/Chart.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Highcharts from 'highcharts';
+import Highcharts, { Options } from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
 import HighchartsSankey from 'highcharts/modules/sankey';
 import HighchartsOrganization from 'highcharts/modules/organization';
@@ -12,7 +12,7 @@ if (typeof Highcharts === 'object') {
 }
 
 const OrgChart = () => {
-  const options = {
+  const options: Options = {
     chart: {
       height: 600,
       inverted: true,
@@ -28,112 +28,294 @@ const OrgChart = () => {
           'reports to {fromNode.id}',
       },
     },
+    tooltip: {
+      enabled: false,
+    },
+    exporting: {
+      allowHTML: true,
+      sourceWidth: 800,
+      sourceHeight: 600,
+    },
     series: [
       {
         type: 'organization',
         // name: '',
         keys: ['from', 'to'],
+        cursor: 'pointer',
+        point: {
+          events: {
+            click: function () {
+              // location.href = this?.options?.custom?.src;
+              console.log(this.options);
+            },
+          },
+        },
         data: [
-          ['Vazir Sh.Shermatov', "Hay'at"],
-          ["Hay'at", 'CEO'],
-          ['CEO', 'CTO'],
-          ['CEO', 'CPO'],
-          ['CEO', 'CSO'],
-          ['CEO', 'HR'],
-          ['CTO', 'Product'],
-          ['CTO', 'Web'],
-          ['CSO', 'Sales'],
-          ['HR', 'Market'],
-          ['CSO', 'Market'],
-          ['HR', 'Market'],
-          ['CTO', 'Market'],
+          {
+            from: 'vazir',
+            to: "Vazir kotibiyati boshlig'i",
+            color: 'red',
+          },
+          {
+            from: 'vazir',
+            to: "Vazirning birinchi o'rinbosari",
+          },
+          // -------------------------------
+          {
+            from: "Vazir kotibiyati boshlig'i",
+            to: 'Axborot-tahlil va ijro intizomi boshqarmasi',
+          },
+          {
+            from: "Vazir kotibiyati boshlig'i",
+            to: "Nazorat va ijro intizomi bo'limi",
+          },
+          {
+            from: "Vazir kotibiyati boshlig'i",
+            to: "Murojaatlar bo'limi",
+          },
+          {
+            from: "Vazir kotibiyati boshlig'i",
+            to: "Yuridik ta'minlash bo'limi",
+          },
+          {
+            from: "Vazir kotibiyati boshlig'i",
+            to: "Xalqaro hamkorlik bo'limi",
+          },
+          // -------------------------------------------
+          {
+            from: "Vazirning birinchi o'rinbosari",
+            to: 'Elektron hukumat tizimini rivojlantirish boshqarmasi',
+          },
+          {
+            from: "Vazirning birinchi o'rinbosari",
+            to: "Davlat organlarini raqamlashtirish bo'limi",
+          },
+          {
+            from: "Vazirning birinchi o'rinbosari",
+            to: "Elektron davlat xizmatlarini joriy etish va idoralararo elektron hamkorlik bo'limi",
+          },
+          {
+            from: "Vazirning birinchi o'rinbosari",
+            to: 'Raqamli iqtisodiyotni rivojlantirish boshqarmasi',
+          },
+          {
+            from: "Vazirning birinchi o'rinbosari",
+            to: "Iqtisodiyot tarmoqlarini raqamlashtirish bo'limi",
+          },
+          {
+            from: "Vazirning birinchi o'rinbosari",
+            to: "Raqamli industriya va IT-eksportni rivojlantirish bo'limi",
+          },
+          {
+            from: "Vazirning birinchi o'rinbosari",
+            to: "Sun'iy intellekt texnologiyalarini joriy qilish va rivojlantirish bo'limi",
+          },
+          // ----------------------------------------
+          {
+            from: 'vazir',
+            to: 'Strategik rivojlantirish boshqarmasi',
+          },
+          {
+            from: 'vazir',
+            to: "Inson resurslarini rivojlantirish va boshqarish bo'limi",
+          },
+          {
+            from: 'vazir',
+            to: "Ichki audit va korrupsiyaga qarshi kurashish bo'limi",
+          },
+          {
+            from: 'vazir',
+            to: 'Axborot xizmati',
+          },
+          {
+            from: 'vazir',
+            to: 'Vazir yordamchisi',
+          },
+          {
+            from: 'vazir',
+            to: 'Iqtisodiyot boshqarmasi',
+          },
+          {
+            from: 'vazir',
+            to: "Buxgalteriya hisobi bo'limi",
+          },
         ],
+        // data: [
+        //   ['vazir Sh.Shermatov', "Hay'at"],
+        //   ["Hay'at", 'CEO'],
+        //   ['CEO', 'CTO'],
+        //   ['CEO', 'CPO'],
+        //   ['CEO', 'CSO'],
+        //   ['CEO', 'HR'],
+        //   ['CPO', 'Product'],
+        //   ['CTO', 'Web'],
+        //   ['CSO', 'Sales'],
+        //   ['HR', 'Market'],
+        //   ['CSO', 'Market'],
+        //   ['CTO', 'Market'],
+        // ],
         levels: [
           {
             level: 0,
-            color: 'silver',
-            dataLabels: {
-              color: 'black',
-            },
-            height: 25,
+            color: '#aaa',
+            // states: {
+            //   hover: {
+            //     color: '#aad',
+            //   },
+            // },
           },
           {
             level: 1,
-            color: '#aae',
-            dataLabels: {
-              color: 'white',
-            },
-            height: 25,
+            color: '#bbb',
           },
           {
             level: 2,
-            color: '#980104',
+            color: '#ccc',
+            borderColor: 'blue',
           },
           {
-            level: 4,
-            color: '#359154',
+            level: 3,
+            color: 'rgba(0,0,0,0.2)',
           },
         ],
         nodes: [
           {
-            id: 'Vazir Sh.Shermatov',
+            id: "Vazir kotibiyati boshlig'i",
+            name: "Vazir kotibiyati boshlig'i",
+            column: 2,
+            level: 2,
           },
           {
-            id: "Hay'at",
+            id: 'Axborot-tahlil va ijro intizomi boshqarmasi',
+            name: 'Axborot-tahlil va ijro intizomi boshqarmasi',
+            column: 2,
           },
           {
-            id: 'CEO',
-            title: 'CEO',
-            name: 'Atle Sivertsen',
-            image:
-              'https://wp-assets.highcharts.com/www-highcharts-com/blog/wp-content/uploads/2022/06/30081411/portrett-sorthvitt.jpg',
+            id: "Nazorat va ijro intizomi bo'limi",
+            name: "Nazorat va ijro intizomi bo'limi",
+            column: 2,
           },
           {
-            id: 'HR',
-            title: 'HR',
-            name: 'Anne Jorunn Fjærestad',
-            color: '#007ad0',
-            image:
-              'https://wp-assets.highcharts.com/www-highcharts-com/blog/wp-content/uploads/2020/03/17131210/Highsoft_04045_.jpg',
+            id: "Murojaatlar bo'limi",
+            name: "Murojaatlar bo'limi",
+            column: 2,
           },
           {
-            id: 'CTO',
-            title: 'CTO',
-            name: 'Christer Vasseng',
-            image:
-              'https://wp-assets.highcharts.com/www-highcharts-com/blog/wp-content/uploads/2020/03/17131120/Highsoft_04074_.jpg',
+            id: "Yuridik ta'minlash bo'limi",
+            name: "Yuridik ta'minlash bo'limi",
+            column: 2,
           },
           {
-            id: 'CPO',
-            title: 'CPO',
-            name: 'Torstein Hønsi',
-            image:
-              'https://wp-assets.highcharts.com/www-highcharts-com/blog/wp-content/uploads/2020/03/17131213/Highsoft_03998_.jpg',
+            id: "Xalqaro hamkorlik bo'limi",
+            name: "Xalqaro hamkorlik bo'limi",
+            column: 2,
           },
           {
-            id: 'CSO',
-            title: 'CSO',
-            name: 'Anita Nesse',
-            image:
-              'https://wp-assets.highcharts.com/www-highcharts-com/blog/wp-content/uploads/2020/03/17131156/Highsoft_03834_.jpg',
+            id: "Vazirning birinchi o'rinbosari",
+            name: "Vazirning birinchi o'rinbosari",
+            column: 3,
+            level: 1
           },
           {
-            id: 'Product',
-            name: 'Product developers',
+            id: 'Elektron hukumat tizimini rivojlantirish boshqarmasi',
+            name: 'Elektron hukumat tizimini rivojlantirish boshqarmasi',
+            column: 3,
+            level: 3,
           },
           {
-            id: 'Web',
-            name: 'Web devs, sys admin',
+            id: "Davlat organlarini raqamlashtirish bo'limi",
+            name: "Davlat organlarini raqamlashtirish bo'limi",
+            column: 3,
+            level: 3,
           },
           {
-            id: 'Sales',
-            name: 'Sales team',
+            id: "Elektron davlat xizmatlarini joriy etish va idoralararo elektron hamkorlik bo'limi",
+            name: "Elektron davlat xizmatlarini joriy etish va idoralararo elektron hamkorlik bo'limi",
+            column: 3,
+            level: 3,
           },
           {
-            id: 'Market',
-            name: 'Marketing team',
-            column: 5,
+            id: 'Raqamli iqtisodiyotni rivojlantirish boshqarmasi',
+            name: 'Raqamli iqtisodiyotni rivojlantirish boshqarmasi',
+            column: 3,
+            level: 3,
           },
+          {
+            id: "Iqtisodiyot tarmoqlarini raqamlashtirish bo'limi",
+            name: "Iqtisodiyot tarmoqlarini raqamlashtirish bo'limi",
+            column: 3,
+            level: 3,
+          },
+          {
+            id: "Raqamli industriya va IT-eksportni rivojlantirish bo'limi",
+            name: "Raqamli industriya va IT-eksportni rivojlantirish bo'limi",
+            column: 3,
+            level: 3,
+          },
+          {
+            id: "Sun'iy intellekt texnologiyalarini joriy qilish va rivojlantirish bo'limi",
+            name: "Sun'iy intellekt texnologiyalarini joriy qilish va rivojlantirish bo'limi",
+            column: 3,
+            level: 3,
+          },
+          {
+            id: 'vazir',
+            name: 'Sh.Shermatov',
+            title: 'Vazir',
+          },
+          // {
+          //   id: 'CEO',
+          //   title: 'CEO',
+          //   name: 'Atle Sivertsen',
+          //   image:
+          //     'https://wp-assets.highcharts.com/www-highcharts-com/blog/wp-content/uploads/2022/06/30081411/portrett-sorthvitt.jpg',
+          // },
+          // {
+          //   id: 'HR',
+          //   title: 'HR',
+          //   name: 'Anne Jorunn Fjærestad',
+          //   color: '#007ad0',
+          //   image:
+          //     'https://wp-assets.highcharts.com/www-highcharts-com/blog/wp-content/uploads/2020/03/17131210/Highsoft_04045_.jpg',
+          // },
+          // {
+          //   id: 'CTO',
+          //   title: 'CTO',
+          //   name: 'Christer Vasseng',
+          //   image:
+          //     'https://wp-assets.highcharts.com/www-highcharts-com/blog/wp-content/uploads/2020/03/17131120/Highsoft_04074_.jpg',
+          // },
+          // {
+          //   id: 'CPO',
+          //   title: 'CPO',
+          //   name: 'Torstein Hønsi',
+          //   image:
+          //     'https://wp-assets.highcharts.com/www-highcharts-com/blog/wp-content/uploads/2020/03/17131213/Highsoft_03998_.jpg',
+          // },
+          // {
+          //   id: 'CSO',
+          //   title: 'CSO',
+          //   name: 'Anita Nesse',
+          //   image:
+          //     'https://wp-assets.highcharts.com/www-highcharts-com/blog/wp-content/uploads/2020/03/17131156/Highsoft_03834_.jpg',
+          // },
+          // {
+          //   id: 'Product',
+          //   name: 'Product developers',
+          // },
+          // {
+          //   id: 'Web',
+          //   name: 'Web devs, sys admin',
+          // },
+          // {
+          //   id: 'Sales',
+          //   name: 'Sales team',
+          // },
+          // {
+          //   id: 'Market',
+          //   name: 'Marketing team',
+          //   column: 5,
+          // },
         ],
         colorByPoint: false,
         color: '#007ad0',
@@ -144,14 +326,6 @@ const OrgChart = () => {
         nodeWidth: 65,
       },
     ],
-    tooltip: {
-      outside: true,
-    },
-    exporting: {
-      allowHTML: true,
-      sourceWidth: 800,
-      sourceHeight: 600,
-    },
   };
 
   return (
