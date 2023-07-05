cc42093cc0bf4327ba92fe00a2daeca3b1a64f69
arslanovngc <arslanov.uzinfocom@gmail.com>
2023-07-05 14:37:47 +0500

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import Head from 'next/head';
+import Chart from '@/components/Chart';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>howdy highcharts</title>
+        <meta name="description" content="howdy highcharts" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>goodbye world</h1>
+        <div className="container">
+          <Chart />
+        </div>
+      </main>
+    </>
+  );
+}
